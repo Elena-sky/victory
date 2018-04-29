a2cbf0b21d470cedbc515cd8d0b0f437300e03a9
MacBook <alonasoroka1993@gmail.com>
2018-04-29 22:51:59 +0300

    view banner page

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/banner',  function () {
+    return view('bannerPage');
 });
