971fd778f880b659b05b34d3a8d2dcd7b1de7417
MacBook <alonasoroka1993@gmail.com>
2018-04-29 19:41:52 +0300

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+/**
+ * Created by MacBook on 28.04.2018.
+ */
+$(document).ready(function () {
+
+    $(".forpublishers-row-one").hover(function () {
+        $('#change').css('color', '#FFFF00');
+        console.log('навела');
+    });
+
+    $(".forpublishers-row-three").hover(function () {
+        $('#change').css('color', '#008000');
+        console.log('навела');
+    });
+
+    $()
+
+
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,253 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+        <title>your victorious ads</title>
+
+        <!-- Fonts -->
+        <script src="https://use.fontawesome.com/29af5d7ea7.js"></script>
+    {{--<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">--}}
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset("css/style.css") }}">
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+
+    </head>
+
+
+
+
+    <body>
+
+    <!-- header -->
+    <header>
+        <section id="header-section" class="header-section">
+            <div class="container">
+
+                <!-- menu -->
+                <div class="row">
+                    <nav class="navbar navbar-expand-md fixed-top container">
+
+                        <button class="navbar-toggler collapsed " type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"><i class="fa fa-bars" aria-hidden="true"></i></span>
+                        </button>
+
+                        <div class="navbar-collapse collapse" id="navbarsExampleDefault" style="">
+                            <ul class="navbar-nav nav-block">
+                                <li class="nav-item active nav-block__nav-item">
+                                    <a class="nav-link disabled nav-block__nav-link" href="#about-section">about</a>
+                                </li>
+                                <li class="nav-item nav nav-block__nav-item">
+                                    <a class="nav-link disabled nav-block__nav-link" href="#forpublishers-section">for publishers</a>
+                                </li>
+                                <li class="nav-item nav-block__nav-item">
+                                    <a class="nav-link disabled nav-block__nav-link" href="#foradvertisers-section">for advertisers</a>
+                                </li>
+                                <li class="nav-item nav-block__nav-item">
+                                    <a class="nav-link disabled nav-block__nav-link" href="#contacts-section">contacts</a>
+                                </li>
+                                <li class="nav-item nav-block__nav-item">
+                                    <a class="nav-link disabled nav-block__nav-link" href="#">sign up</a>
+                                </li>
+                                <li class="nav-item nav-block__nav-item">
+                                    <a class="nav-link disabled nav-block__nav-link" href="#">login</a>
+                                </li>
+                            </ul>
+
+                        </div>
+                    </nav>
+                </div>
+                <!-- End menu -->
+
+
+                <!-- image and text -->
+                <div class="row you-victorious-row">
+                    <div class="col-md-6 offset-md-6 you-victorious-block">
+                        <img class="you-victorious-block__img" src="dist/img/logo.svg"  alt="">
+                        <h1 class="you-victorious-block__title">your victorious ads</h1>
+                        <a class="you-victorious-block__signup" href="#">sign up</a>
+                    </div>
+                </div>
+                <!-- End image and text -->
+
+            </div>
+        </section>
+
+    </header>
+    <!-- End header -->
+
+        {{--<div class="flex-center position-ref full-height">--}}
+            {{--@if (Route::has('login'))--}}
+                {{--<div class="top-right links">--}}
+                    {{--@auth--}}
+                        {{--<a href="{{ url('/home') }}">Home</a>--}}
+                    {{--@else--}}
+                        {{--<a href="{{ route('login') }}">Login</a>--}}
+                        {{--<a href="{{ route('register') }}">Register</a>--}}
+                    {{--@endauth--}}
+                {{--</div>--}}
+            {{--@endif--}}
+
+            {{--<div class="content">--}}
+                {{--<div class="title m-b-md">--}}
+                    {{--Laravel--}}
+                {{--</div>--}}
+
+                {{--<div class="links">--}}
+                    {{--<a href="https://laravel.com/docs">Documentation</a>--}}
+                    {{--<a href="https://laracasts.com">Laracasts</a>--}}
+                    {{--<a href="https://laravel-news.com">News</a>--}}
+                    {{--<a href="https://forge.laravel.com">Forge</a>--}}
+                    {{--<a href="https://github.com/laravel/laravel">GitHub</a>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+
+
+    <!-- main block -->
+    <main class="main-block">
+
+        <!-- about section -->
+        <section id="about-section" class="about-section">
+            <div class="container">
+                <div class="row about-row-one">
+                    <div class="col-md-6 about-row-one__text">
+                        We are here for you to make your advertising work. For several years we carefully studied the mobile
+                        advertising market, learned all its secrets, strengths and weaknesses. We always pay great attention to
+                        the subtleties and nuances, and due to this, we can direct the mobile advertising as quickly and
+                        efficiently as possible
+                    </div>
+                </div>
+                <div class="row about-row-two">
+                    <div class="col-md-6 offset-md-6 about-row-two__text">
+                        We provide our customers with a high-quality service based on the latest technologies and in-depth
+                        data analytics. The customization and flexibility of our platform will allow you to discover the new
+                        highest standards for CPI / CPА / CPM advertising campaigns.
+                    </div>
+                </div>
+                <div class="row about-row-three">
+                    <div class="col-md-6 about-row-three__text">
+                        The team of our highly motivated specialists of the highest level is ready to immediately help you,
+                        answer all the questions that arise and help both advertisers and publishers achieve maximum results
+                        in a short time.
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End about section -->
+
+        <!-- for publishers section -->
+        <section id="forpublishers-section" class="forpublishers-section">
+            <div class="container">
+                <div class="background-text change" id="change">
+                    <p><span>FOR</span></br></span>PUBLISHERS</p>
+                </div>
+                <div class="row forpublishers-row-one">
+                    <div class="col-md-6 offset-md-6 forpublishers-row-one__text">
+                        The main goal of each publisher is to adjust the flow of their mobile traffic so that they bring
+                        the maximum profit. Do you still think that this is easier said than done? You tried a lot of
+                        options, but none of the platforms brought you the planned result? Do you think that other publishers
+                        are successful because of some magic or a secret recipe? We know this secret and can make it work for you.
+                    </div>
+                </div>
+                <div class="row forpublishers-row-three">
+                    <div class="col-md-6 forpublishers-row-three__text">
+                        Working with us you choose only among the best offers. We will provide you with the most accurate
+                        and up-to-date tracking system in real time and API integration in a few easy steps. Our team of
+                        experts is always here to help.
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End for publishers  section-->
+
+        <!-- for advertisers section -->
+        <section id="foradvertisers-section" class="foradvertisers-section">
+            <div class="container">
+                <div class="row foradvertisers-row-one">
+                    <div class="col-md-6 offset-md-6 foradvertisers-row-one__text">
+                        To deliver your Ads to every potential customer. To hook
+                        on the narrowest niches and the most hard-to-reach
+                        audiences. Advertisers face these challenges every day and
+                        often they determine the success of the camping. You
+                        think that you have tried everything, but the result still does
+                        not please you? We are here to come to your aid.
+                    </div>
+                </div>
+                <div class="row foradvertisers-row-three">
+                    <div class="col-md-6 foradvertisers-row-three__text">
+                        The most accurate targeting. Fine-tuned Ad campaigns. The
+                        best anti-fraud technologies. Direct access to the most
+                        succesful publishers and high-priority trafic. All these
+                        factors will work for you, bring you market leadership and
+                        impressively increase your ROI.
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End for advertisers section -->
+
+    </main>
+    <!-- End main block -->
+
+
+    <footer>
+        <section id="contacts-section" class="contacts-section">
+            <div class="container">
+                <!-- contacts -->
+                <div class="row contacts-row-one">
+                    <div class="col-md-6 offset-md-6 contacts-row-one__text">
+                        Are you a publisher and want to turn your traffic into gold?
+                        Are you an advertiser and want your advertising campaign
+                        to clearly reach the customers? In both cases, you are in
+                        the right place.
+                    </div>
+                </div>
+
+                <div class="background-text">
+                    <p><span>CONTACTS</span></p>
+                </div>
+
+                <div class="row contacts-row-two">
+                    <div class="col-md-6  contacts-row-two__text">
+                        Do not delay, start making profits today! Just fill out the
+                        from below and our team will contact you as
+                        soon as possible.
+                    </div>
+                </div>
+
+
+                <!-- End contacts -->
+
+                <!-- contacts form -->
+                <div class="row contacts-row-three">
+                    <div class="col-md-6 offset-md-6 contacts-row-three__form">
+                        <form  class="contact-form" action="">
+                            <input class="contact-form__name" type="text" placeholder="You name"/>
+                            <input class="contact-form__email" type="text" placeholder="Email"/>
+                            <textarea rows="4" class="contact-form__message" type="text" placeholder="Message"></textarea>
+                            <input class="contact-form__submit btn" type="submit" value="submit"/>
+                        </form>
+                    </div>
+                </div>
+                <!-- End contacts form -->
+
+            </div>
+        </section>
+    </footer>
+
+    </body>
+
+    <script src="{{ asset("js/script.js") }}"></script>
+
+</html>
